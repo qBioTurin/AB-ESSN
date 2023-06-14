a1fe495a9cd3d145b882c17dbaf54f60b50a3057
PerniceSimone <simone.pernice@unito.it>
2023-06-14 09:17:23 +0200

    Updating the scripts

--- SEIR/SSA/Rfunction/FiguresGeneration.R
+++ SEIR/SSA/Rfunction/FiguresGeneration.R
@@ -44,7 +44,7 @@ pl = ReInfectionTimesDF %>%
   geom_violin(aes(x = Ninfection, y = Time, col = Deceased, fill = Deceased),alpha= 0.2) +
   scale_fill_manual(values = c("Dead" = col[1], "Alive" = col[2])) +
   scale_color_manual(values = c("Dead" = col[1], "Alive" = col[2])) +
-  labs(x = "Number of infections", 
+  labs(x = "(Re)Infection number - single simulation run", 
        y = "Time of the infection (days)", 
        col = "Status",
        fill = "Status") +
@@ -65,21 +65,21 @@ pl = ReInfectionTimesDF %>%
   geom_violin(aes(x = Ninfection, y = Time, col =  "blue", fill = "blue"), alpha= 0.2) +
   scale_fill_manual(values =  col[2] ) +
   scale_color_manual(values = col[2] ) +
-  labs(x = "Number of infections", 
+  labs(x = "(Re)Infection number - single simulation run", 
        y = "Time of the infection (days)", 
        col = "Status",
        fill = "Status") +
   theme_bw()+
   theme(legend.position = "none",
-        axis.title = element_text(face="bold",size = 15),
+        axis.title = element_text(face="bold",size = 18),
         legend.title = element_text(face="bold",size = 15),
-        axis.text = element_text(size = 10),
+        axis.text = element_text(size = 12),
         legend.text = element_text(size = 12),
         strip.text = element_text(face="bold",size = 12))
 
 pl
 
-ggsave(pl,filename = "InfectionTimes.pdf",device = "pdf",width = 8,height = 5)
+ggsave(pl,filename = "Plots/InfectionTimes_revised.pdf",device = "pdf",width = 8,height = 5)
 
 pl =  ReInfectionTimesDF %>%
   gather(-Deceased,-IDagent, value = "Time", key = "Ninfection") %>% 
@@ -116,29 +116,26 @@ pl =  ReInfectionTimesDF %>%
   ungroup() %>%
   group_by(File,Deceased,n) %>%
   dplyr::summarise(Ninfection = length(n)) %>%
-  #filter(Deceased=="Alive") %>%
+  filter(Deceased=="Alive") %>%
   ggplot() +
   geom_boxplot(aes(x = as.factor(n), y = Ninfection,
                    col = "blue", fill = "blue"),
                alpha = 0.4) +
   scale_fill_manual(values =  "#44015480" ) +
   scale_color_manual(values = "#44015480" ) +
-  labs(x = "Number of infections",
+  labs(x = "(Re)Infection number - 1000 simulation runs",
        y = "Distribution of counts",
        fill = "Status") +
   theme_bw()+
   theme(legend.position = "none",
-        axis.title = element_text(face="bold",size = 15),
+        axis.title = element_text(face="bold",size = 18),
         legend.title = element_text(face="bold",size = 15),
-        axis.text = element_text(size = 10),
+        axis.text = element_text(size = 12),
         legend.text = element_text(size = 12),
         strip.text = element_text(face="bold",size = 12))
 pl
 
-ggsave(pl,filename = "Plots/InfectionCounts.pdf",device = "pdf",width = 8,height = 5)
-
-
-
+ggsave(pl,filename = "Plots/InfectionCounts_revised.pdf",device = "pdf",width = 8,height = 5)
 
 
 
